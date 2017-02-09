2b01cc88f5b3970c1abb176e7ac96c589d0388ee
Triss <git@webkandii.com>
2017-02-09 12:12:46 +0000

    added artists folder

---
+++ artists/inc/functions.php
@@ -0,0 +1,221 @@
+<?php
+	// Function to display records from database
+		// Type is for the amount columns
+	function display_records($type, $conn, $table) {
+		$data_array = array();
+
+		if($type == 'default') {
+			$get_data = $conn->prepare('SELECT * FROM `' . $table . '` WHERE `visible` = 1');
+	        $get_data->execute();
+	        $count = $conn->query('SELECT FOUND_ROWS()')->fetchColumn();
+	    	
+	    	if($count > '0') {
+		        while ($data = $get_data->fetch()) {
+		            $id = $data['id'];
+		            $name = $data['name'];
+		            $title = $data['title'];
+		            $company = $data['company'];
+		            $address = $data['address'];
+		            $city = $data['city'];
+		            $postcode = $data['postcode'];
+		            $tel = $data['email'];
+		            $email = $data['tel'];
+		            $web = $data['web'];
+		            $notes = $data['notes'];
+		            $owned_by = $data['owned_by'];
+		            $return_url = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] . '&hash=' . $table;
+		            $actions = '';
+		            $class = '';
+		            $ab_table = 'ab_' . $table;
+
+		            if(logged_in() == true) {
+		            	if(in_address_book($id, $_SESSION[$ab_table]) == true) {
+		            		$actions = '<a href="#" class="remove-from-address-book show-tooltip" data-id="' . $id . '" data-table="' . $table . '" data-del="false" original-title="Remove ' . $name . ' from address book"></a>';
+		            		$class = ' class="in-address-book"';
+		            	} else {
+		            		$actions = '<a href="#" class="add-to-address-book show-tooltip" data-id="' . $id . '" data-table="' . $table . '" original-title="Add ' . $name . ' to address book"></a>';
+		            	}
+
+		            	if($_SESSION['permissions'] == 'admin' || $owned_by == $_SESSION['id']) {
+		            		$actions .= '<a href="edit.php?r=' . $id . '&table=' . $table . '&return=' . $return_url . '" class="edit-record"></a> <a href="delete.php?r=' . $id . '&table=' . $table . '&return=' . $return_url . '" onclick="return confirm("Are you sure you want to delete this record (ID ' . $id . ') from ' . $table . '?")" class="delete-record"></a>';
+		            	}
+		            }
+
+		            $row = <<<row
+					    <tr$class>
+					        <td>$name</td>
+					        <td>$title</td>
+					        <td>$company</td>
+					        <td>$address</td>
+					        <td>$city</td>
+					        <td>$postcode</td>
+					        <td><a href="mailto:$email">$email</a></td>
+					        <td>$tel</td>
+					        <td><a href="$web">$web</a></td>
+					        <td>$notes</td>
+					        <td class="actions-td"><div>$actions</div></td>
+					    </tr>
+row;
+					// Add row to data_array to allow all records in database to be looped through
+					array_push($data_array, $row);
+		        }
+
+		        return $data_array;
+		    } else {
+		    	return '<p><strong>We could not find any records.</strong></p>';
+		    }
+	    } else {
+	    	return '<p><strong>We could not find any records. Reason: Not all function variables are declared.</strong></p>';
+	    }
+	}
+
+	function display_address_book($type, $conn, $table) {
+		$data_array = array();
+		$ab_table = 'ab_' . $table;
+
+		$get_data = $conn->prepare('SELECT * FROM `' . $table . '` WHERE `id` IN(' . $_SESSION[$ab_table] . ')');
+        $get_data->execute(); 
+    
+        while ($data = $get_data->fetch()) {
+            $id = $data['id'];
+            $name = $data['name'];
+            $title = $data['title'];
+            $company = $data['company'];
+            $address = $data['address'];
+            $city = $data['city'];
+            $postcode = $data['postcode'];
+            $tel = $data['email'];
+            $email = $data['tel'];
+            $web = $data['web'];
+            $notes = $data['notes'];
+            $owned_by = $data['owned_by'];
+            $return_url = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] . '&hash=' . $table;
+        	$actions = '<a href="#" class="remove-from-address-book show-tooltip" data-id="' . $id . '" data-table="' . $table . '" data-del="true" original-title="Remove ' . $name . ' from address book"></a>';
+
+			if($owned_by == $_SESSION['id']) {
+        		$actions .= '<a href="edit.php?r=' . $id . '&table=' . $table . '&return=' . $return_url . '" class="edit-record"></a><a href="delete.php?r=' . $id . '&table=' . $table . '&return=' . $return_url . '" onclick="return confirm("Are you sure you want to delete this record (ID ' . $id . ') from ' . $table . '?")" class="delete-record"></a>';
+        	}
+
+            $row = <<<row
+        <tr>
+            <td>$name</td>
+            <td>$title</td>
+            <td>$company</td>
+            <td>$address</td>
+            <td>$city</td>
+            <td>$postcode</td>
+            <td><a href="mailto:$email">$email</a></td>
+            <td>$tel</td>
+            <td><a href="$web">$web</a></td>
+            <td>$notes</td>
+            <td class="actions-td"><div>$actions</div></td>
+        </tr>
+row;
+            // Add row to data_array to allow all records in database to be looped through
+			array_push($data_array, $row);
+        }
+
+        return $data_array;
+	}
+
+	// Mysql_real_escape_string equivalent 
+	function protect($value) {
+	    $search = array("\\",  "\x00", "\n",  "\r",  "'",  '"', "\x1a");
+	    $replace = array("\\\\","\\0","\\n", "\\r", "\'", '\"', "\\Z");
+
+	    return str_replace($search, $replace, $value);
+	}
+
+	function get_category_url($categories, $category) {
+		foreach ($categories as $row) {
+			if($row['category'] == $category) {
+				return $row['url'];
+			}
+		}
+	}
+
+	function get_category_name($categories, $category) {
+		foreach ($categories as $row) {
+			if($row['category'] == $category) {
+				return $row['name'];
+			}
+		}
+	}
+
+	function create_salt() {
+	    $string = md5(uniqid(rand(), true));
+	    return substr($string, 0, 3);
+	}
+
+	function validate_user($id, $first_name, $last_name, $email, $permissions, $ab_count, $ab_agents, $ab_worldwide, $ab_promoters, $ab_digital, $ab_print, $ab_broadcast, $ab_venues, $ab_london, $ab_festivals, $ab_nonukfests, $ab_tech) {
+	    session_regenerate_id (); // Security measure
+	    $_SESSION['valid'] = 1;
+	    $_SESSION['id'] = $id;
+	    $_SESSION['first_name'] = $first_name;
+	    $_SESSION['last_name'] = $last_name;
+	    $_SESSION['email'] = $email;
+	    $_SESSION['permissions'] = $permissions;
+	    $_SESSION['ab_count'] = $ab_count;
+        $_SESSION['ab_agents'] = $ab_agents;
+        $_SESSION['ab_worldwide'] = $ab_worldwide;
+        $_SESSION['ab_promoters'] = $ab_promoters;
+        $_SESSION['ab_digital'] = $ab_digital;
+        $_SESSION['ab_print'] = $ab_print;
+        $_SESSION['ab_broadcast'] = $ab_broadcast;
+        $_SESSION['ab_venues'] = $ab_venues;
+        $_SESSION['ab_london'] = $ab_london;
+        $_SESSION['ab_festivals'] = $ab_festivals;
+        $_SESSION['ab_national'] = $ab_national;
+        $_SESSION['ab_nonukfests'] = $ab_nonukfests;
+        $_SESSION['ab_tech'] = $ab_tech;
+	}
+
+	function logged_in() {
+	    if(isset($_SESSION['valid']) && $_SESSION['valid']) {
+	        return true;
+	    } else {
+	    	return false;
+	    }
+	}
+
+	function logout() {
+	    $_SESSION = array(); // Destroy all of the session variables
+	    session_destroy();
+	}
+
+	function in_multiarray($key, $value, $array) {
+	    $top = sizeof($array) - 1;
+	    $bottom = 0;
+	    while($bottom <= $top)
+	    {
+	        if($array[$bottom][$key] == $value)
+	            return true;
+	        else 
+	            if(is_array($array[$bottom][$key]))
+	                if(in_multiarray($value, ($array[$bottom][$key])))
+	                    return true;
+
+	        $bottom++;
+	    }        
+	    return false;
+	}
+
+	// Function checks whether an ID already exists in users address book
+	function in_address_book($rid, $ab_table) {
+		$check_exists = explode(', ', $ab_table);
+	    if(in_array($rid, $check_exists)) {
+	    	return true;
+	    } else {
+	    	return false;
+	    }
+	}
+
+	function count_arr($arr) {
+		if(!array_filter($arr)) {
+		 	// array contains only empty values
+			return '0';
+		} else {
+			return count($arr);
+		}
+	}
+?>
---
+++ artists/inc/header.php
@@ -0,0 +1,164 @@
+<?php
+    if(isset($_POST['login'])) {
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        if(!$email || !$password) {
+            $login_err = 'Please enter a valid email address and password.';
+        } else {
+            try {
+                $check = $conn->prepare('SELECT * FROM `users` WHERE `email` = :email');
+                $check->bindParam(':email', $email);
+                $check->execute();
+                while ($data = $check->fetch()) {
+                    $db_id = $data['id'];
+                    $db_password = $data['password'];
+                    $db_salt = $data['salt'];
+                    $db_first_name = $data['first_name'];
+                    $db_last_name = $data['last_name'];
+                    $db_permissions = $data['permissions'];
+                    $db_ab_count = $data['ab_count'];
+                    $db_ab_agents = $data['ab_agents'];
+                    $db_ab_worldwide = $data['ab_worldwide'];
+                    $db_ab_promoters = $data['ab_promoters'];
+                    $db_ab_digital = $data['ab_digital'];
+                    $db_ab_print = $data['ab_print'];
+                    $db_ab_broadcast = $data['ab_broadcast'];
+                    $db_ab_venues = $data['ab_venues'];
+                    $db_ab_london = $data['ab_london'];
+                    $db_ab_festivals = $data['ab_festivals'];
+                    $db_ab_nonukfests = $data['ab_nonukfests'];
+                    $db_ab_tech = $data['ab_tech'];
+                    $count = count($data);
+                }
+
+                if($count >= 1) { // Check email address is valid
+                    $hash = hash('sha256', $db_salt . hash('sha256', $password));
+                
+                    if($hash != $db_password) { // Check password 
+                        $login_err = 'Please enter a valid email address and password.';
+                    } else {
+                        validate_user($db_id, $db_first_name, $db_last_name, $email, $db_permissions, $db_ab_count, $db_ab_agents, $db_ab_worldwide, $db_ab_promoters, $db_ab_digital, $db_ab_print, $db_ab_broadcast, $db_ab_venues, $db_ab_london, $db_ab_festivals, $db_ab_nonukfests, $db_ab_tech);
+                        header('Location: ' . $website_path . 'address-book.php');
+                    }
+                } else {
+                   $login_err = 'Please enter a valid email address and password.';
+                }
+            } catch (PDOException $e) {
+                $login_err = 'Could not login, please contact us' . $e->getMessage();
+            }
+        }
+    }
+?>
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title><?php echo $page_title; ?></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+        <link rel="stylesheet" href="../css/normalize.css">
+        <link rel="stylesheet" href="../css/main.css">
+        <link rel="stylesheet" href="../css/datatables.css">
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <header <?php if(isset($page) && $page == 'home') { echo 'class="home"'; } ?>>
+            <div class="container">
+                <?php if(logged_in() == true) { ?>
+                <div class="address-book-button">
+                    <a href="<?php echo $website_path; ?>address-book.php">Address Book (<span class="address-book-count"><?php echo $_SESSION['ab_count']; ?></span>)</a>
+                </div>
+                <?php } ?>
+                <div class="top">
+                    <div class="logo">
+                        <a href="<?php echo $website_path; ?>"><img src="<?php echo $website_path; ?>img/logo.png" alt="Musikandi, Logo"></a>
+                    </div>
+                    <nav>
+                        <ul>
+                            <li><a href="<?php echo $website_path; ?>" <?php if(isset($page) && $page == 'home') { echo 'class="active"'; } ?>>Home</a></li>
+                            <li><a href="<?php echo $website_path; ?>bands.php" <?php if(isset($page) && $page == 'bands') { echo 'class="active"'; } ?>>Bands</a></li>
+                            <li><a href="<?php echo $website_path; ?>industry.php" <?php if(isset($page) && $page == 'industry') { echo 'class="active"'; } ?>>Industry</a></li>
+                            <li><a href="<?php echo $website_path; ?>media.php" <?php if(isset($page) && $page == 'media') { echo 'class="active"'; } ?>>Media</a></li>
+                            <li><a href="<?php echo $website_path; ?>venues.php" <?php if(isset($page) && $page == 'venues') { echo 'class="active"'; } ?>>Venues</a></li>
+                            <?php if(logged_in() == true) { ?>
+                            <li class="dropdown">
+                                <a href="#">Hello, <?php echo $_SESSION['first_name']; ?></a>
+                                <div>
+                                    <a href="<?php echo $website_path; ?>address-book.php">Address book</a>
+                                    <a href="<?php echo $website_path; ?>add-contacts.php">Add contacts</a>
+                                    <a href="<?php echo $website_path; ?>import-contacts.php">Import contacts</a>
+                                    <?php if(logged_in() == false && isset($_SESSION['permissions']) && $_SESSION['permissions'] == 'admin') { ?><a href="<?php echo $website_path; ?>info.php">Add records</a><?php } ?>
+                                    <a href="<?php echo $website_path; ?>logout.php">Logout</a>
+                                </div>
+                            </li>
+                            <?php } else { ?>
+                            <li><a href="<?php echo $website_path; ?>register.php" <?php if(isset($page) && $page == 'register') { echo 'class="active"'; } ?>>Register</a></li>
+                            <li class="dropdown">
+                                <a href="#" <?php if(isset($_POST['login'])) { echo 'class="active"'; } ?>>Login</a>
+                                <div <?php if(isset($_POST['login'])) { echo 'style="display: block;"'; } ?>>
+                                    <?php if(isset($login_err)) { echo '<p>' . $login_err . '</p>'; }  ?>
+                                    <form method="POST">
+                                        <ul class="clearfix">
+                                            <li>
+                                                <label for="email">Email address:</label>
+                                                <input type="text" name="email" id="email" <?php if(isset($_POST['email'])) { echo 'value="' . $_POST['email'] . '"'; } ?>>
+                                            </li>
+                                            <li>
+                                                <label for="password">Password:</label>
+                                                <input type="password" name="password" id="password">
+                                            </li>
+                                            <li>
+                                                <input type="submit" value="Log In" name="login">
+                                            </li>
+                                        </ul>
+                                    </form>
+                                </div>
+                            </li>
+                            <?php } ?>
+                        </ul>
+                    </nav>
+                </div>
+                <?php if(isset($page) && $page == 'home') { ?>
+                <div class="bottom-home">
+                    <h1>A free resource for bands, media and music professionals</h1>
+                    
+                    <p>This site is a series of lists. They're my contacts from when I worked full-time in music a few years back. Don't see it as an up to the minute database, but as a guide. Please don't spam people. They won't appreciate it. Use this site to start building your own lists of contacts. If you'd like to add to it or update entries, ask me for a login code.</p>
+                
+                    <div class="search">
+                        <form method="GET" action="<?php echo $website_path; ?>search.php">
+                            <ul>
+                                <li><input type="text" name="q" placeholder="Search for anything! Booking agent, Journalist, Venue..."></li>
+                                <li><input type="submit" value="Search"></li>
+                            </ul>
+                        </form>
+                    </div>
+                </div>
+                <?php } else { ?>
+                <div class="bottom-other">
+                    <div class="breadcrumb">
+                        <p>You are here: <?php echo $breadcrumb; ?></p>
+                    </div>
+                    <div class="search">
+                        <form method="GET" action="<?php echo $website_path; ?>search.php">
+                            <ul>
+                                <li><input type="text" name="q" placeholder="Search for anything! Booking agent, Journalist, Venue..." <?php if(isset($_GET['q'])) { echo 'value="' . $_GET['q'] . '"'; } ?>></li>
+                                <li><input type="submit" value="Search"></li>
+                            </ul>
+                        </form>
+                    </div>
+                </div>
+                <?php } ?>
+            </div>
+        </header>
