ef1e100c7b0860d9ae06dd18e602464d03774b54
Triss <git@webkandii.com>
2017-02-17 14:48:49 +0000

    added floating back-to-top code lines 15-33

--- js/main.js
+++ js/main.js
@@ -11,6 +11,26 @@ $(document).ready(function() {
 		e.preventDefault();
 	   $('html, body').animate({scrollTop: 0}, 1000);
 	});
+	
+	 // Floating back to top - added by Triss - Feb 2017
+        $('body').prepend('<a href="#" class="back-to-top">Back to Top</a>');
+
+        var amountScrolled = 300;
+
+        $(window).scroll(function() {
+	   if ( $(window).scrollTop() > amountScrolled ) {
+		$('a.back-to-top').fadeIn('slow');
+	   } else {
+		$('a.back-to-top').fadeOut('slow');
+	   }
+        });
+
+        $('a.back-to-top, a.simple-back-to-top').click(function() {
+	   $('html, body').animate({
+		scrollTop: 0
+	   }, 700);
+	   return false;
+        });
 
 	// Tabs
 	// If a hash (#) exists in URL then display the hash value instead of defined active element
