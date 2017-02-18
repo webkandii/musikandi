cb90533821d9b3272b00e9ba4ec6ed9a13425ad2
Triss <git@webkandii.com>
2017-02-18 12:24:50 +0000

    Add files via upload

--- inc/about.php
+++ inc/about.php
@@ -1,13 +1,12 @@
 <section id="content">
         <div class="container">
-           
                  <div class="news-headline">How does it work?</div>
 
-<div class="news-body">You can either browse the contacts and take whatever information you need, or take a more active part by registering.</br></br>Once you register you can create your own address book which you can either keep private, or share your contacts with other users.</br></br>This site isn't maintained or kept updated by one person. This means that you can play your part by updating contact information, and by adding new contacts.</div>
-
-  <div class="two-column-1 sub-headline">About</div>
-  <div class="two-column-2 sub-headline">Why?</div>
-<div class="two-column-1"><p class="sub-body links">Musikandi was created by <a href="http://www.webkandii.com">Tris Dickin</a> as a place to keep his music industry contacts. When Tris decided to study and then work in web design, he had less time to keep these contacts updated. Left with a choice of either allowing them to become out of date, or giving them away, he decided to open them up to the public and created this website with all its features that allows editing and privacy.</p></div><div class="two-column-2"><p class="sub-body">The information here is open source and can be found by searching the web. Before Google, music industry contacts were hard to collect. While this list is not comprehensive, it is categorised into sections that users might find useful.</p></div>
-         
+<div class="news-body">You can either browse the contacts and take whatever information you need, or take a more active part by registering.
+</br></br>Once you register you can create your own address book which you can either keep private, or share your contacts with other users.</br></br>This site isn't maintained or kept updated by one person.
+This means that you can play your part by updating contact information, and by adding new contacts.</div>
+Musikandi was created by <a href="http://www.webkandii.com">an ex-music industry professional</a> as a place to keep his contacts.
+Left with a choice of either allowing them to become out of date, or giving them away, he decided to open them up to the public and created this website with all its features that allows editing and privacy.
+Musikandi is open source. Go to https://github.com/webkandii/musikandi to suggest and code changes to the site.</div>
         </div>
 </section>
