4d3dc822a2887fb33306aae9f88fa57bafaca458
Triss <git@webkandii.com>
2017-02-18 18:10:21 +0000

    Update snind.php

--- inc/snind.php
+++ inc/snind.php
@@ -1,5 +1,5 @@
 <div class="alt-three-column">
-<div class="sub-news-box"><div class="sub-news-headline">What's up..</div>
+<div class="sub-news-box">
 <div class="sub-news-headline">Booking Agents: Agency websites</div>
 
 <!-- PIC -->
@@ -12,20 +12,18 @@
 <!-- NEWS -->
 
 <div class="three-column-23">
-<div id="booking-agents" class="data-tab-content active table-pagination">
-                    <h2>Agents Websites</h2>
+<div id="agentswebsites" class="data-tab-content table-pagination">
                     <table>
                         <thead>
                             <tr>
-                                <th>Website</th>
-                                 <?php if(logged_in() == true) { ?>
+                                <?php if(logged_in() == true) { ?>
                                 <th>Actions</th>
                                 <?php } ?>
                             </tr>
                         </thead>
                         <tbody>
                             <?php
-                                $records = display_records('default', $conn, 'agents-websites');
+                                $records = display_records('default', $conn, 'agentswebsites');
                                 foreach ($records as $key => $row) {
                                     echo $row;
                                 }
